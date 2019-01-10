91512e2be9fb02dc31f74699573fb282634fc0af
Reviako Vladislv <vreviaka@gmail.com>
2019-01-10 21:20:06 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Weather</h1>
+    );
+  }
+}
+
+export default App;
