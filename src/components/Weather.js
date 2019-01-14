5fcc71b825eb217e17628e7f0f5e041c8842117f
Reviako Vladislv <vreviaka@gmail.com>
2019-01-15 00:24:18 +0300

    weather css. Flex

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import "../index.css"
+import "../styles/weather.css"
+
+/*TODO ---- rename component form current ----*/
 
 const Weather = (props) => {
   return (
-    <div>
-      {props.temp && <p>temperature: {Math.round(props.temp) + "º"}</p>}
-      {props.humidity && <p>humidity: {props.humidity + "%"}</p>}
-      {props.description && <p>description: {props.description}</p>}
-      {props.error && <p>{props.error}</p>}
+    <div className="current">
+
+      <div className="block">
+        {props.temp && <p className="current__temperature">{Math.round(props.temp) + "º"}</p>}
+      </div>
+
+      <div className="block">
+        {props.humidity && <p className="current__humidity">Влажность: {props.humidity + "%"}</p>}
+        {props.description && <p className="current__precipitation">Осадки: {props.description}</p>}
+        {props.error && <p>{props.error}</p>}
+      </div>
+
     </div>
   )};
 
---
+++ src/components/router/OneDay.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const OneDay = (props) => (
+    <h1>That is weather for ONE DAY</h1>
+  );
+
+
+export default OneDay;
